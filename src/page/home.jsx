e154d809ac21a07dedcd0c526c19922ceec796d7
Centi <ricardo1centy@gmail.com>
2022-09-05 10:43:27 -0600

    nuevo reworw de home

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -5,148 +5,74 @@ import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import Banner from "../components/banner";
 import imgprueba from "../img/imagendeprueba.png";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Target from "../components/targeta";
 import "../css/jumbotron.css";
-import breakI from "../icons/break1.png";
-import engine from "../icons/engine1.png";
-import spray from "../icons/spray-paint.png";
-import truck from "../icons/semi.png";
-import car from "../icons/car.png";
-import forklift from "../icons/forklift.png";
-import towtruck from "../icons/tow-truck(1).png";
 import Footer from "../components/Footer";
+import car from "../icons/electric-car.png";
+import calidad from "../icons/calidad.png";
+import cinsurae from "../icons/car-insurance.png";
+import newcar from "../icons/new-car.png";
+
+
+
+
 
 const Home = () => {
   return (
     <Layout>
       <Banner />
-      <Container fluid="md">
         <br />
-        <center>
-          <h5 className="tema">
-            <i className="distintivo">En FITHO AUTOPARTS.</i>Ofrecemos servicios
-            como:
-          </h5>
-        </center>
-
-        <h3 className="tema">
-          <center>Multiservicios Pacheco</center>
-        </h3>
+        <div className="Nestandares">
+          <Container>
         <Row>
-          <Col>
-            <p>
-              Contamos con cotizaciones gratuitas, precios accesibles,
-              promociones mensuales, garantía, servicio y recolección de
-              vehículos a domicilio.
-            </p>
-            <p>
-              Detectamos y Satisfacemos las necesidades de nuestros clientes,
-              superando siempre sus expectativas de calidad y servicio a un
-              precio justo, con el compromiso de honestidad y transparencia
-              permanentes. Evitando siempre pretextos, llegaremos siempre a dar
-              soluciones y respuestas acertadas, para lograr tener única y
-              exclusivamente clientes satisfechos.
-            </p>
-            <center><Button>Visitar</Button></center>
-            <br />
-          </Col>
-          <Col>
-            <Row>
-              <Col xs lg="2">
-                {" "}
-                <img src={breakI} className="imgicons" />
-              </Col>
+          
+            <Col >
+              <Row>
+                <center className="estandares">NUESTROS ESTANDARES</center>
+              </Row>
+              <br />
+              <Row>
               <Col>
-                {" "}
-                <h5> Cambios de pastillas.</h5>
-              </Col>
-            </Row>
-            <br />
-            <Row>
-              <Col xs lg="2">
-                <img src={engine} className="imgicons" />
-              </Col>
+                  <center><img src={car} alt=""  className="imgicons"/></center>
+                  <h5 className="temai">IMPORTACION</h5>
+                  <p className="parrafo">Importamos vehículos desde Estados Unidos, de acuerdo a los requerimientos de nuestros clientes.</p>
+                </Col>
+                <Col>
+                <center><img src={cinsurae} alt=""  className="imgicons"/></center>
+                <h5 className="temai">MODELOS RECIENTES</h5>
+                  <p className="parrafo">Vehículos desde año 2014, marcas comerciales reconocidas versión americana.</p>
+                </Col>
+              </Row>
+              <Row>
               <Col>
-                <h5> Cambios de aceite.</h5>
-              </Col>
-            </Row>
-            <br />
-            <Row>
-              <Col xs lg="2">
-                <img src={spray} className="imgicons" />
-              </Col>
-              <Col>
-                <h5> Enderezado y pintura.</h5>
-              </Col>
-            </Row>
-          </Col>
+              <center><img src={calidad} alt=""  className="imgicons"/></center>
+              <h5 className="temai">CALIDAD</h5>
+                  <p className="parrafo">Utilizamos repuestos e insumos acorde a la calidad del fabricante, cuando se trabaja con calidad se puede hablar de garantía.</p>
+                </Col>
+                <Col>
+                <center><img src={newcar} alt=""  className="imgicons"/></center>
+                <h5 className="temai">COMPRA DE VEHICULOS EN SUBASTAS</h5>
+                  <p className="parrafo">Se compran los vehiculos a la necesidad y gustos del cliente.</p>
+                </Col>
+              </Row>
+            </Col>
+            
+          
+          <Col></Col>
+          
         </Row>
-      </Container>
+        </Container>
+        </div>
+      
       <div className="jumbotron">
         <Container>
-          <center>
-            <h2 className="tema">Importadora FITOH AutoParts</h2>
-          </center>
-          <br />
-          <center>
-            <p>
-              <center>
-                <Col xs={8}>
-                      Empresa líder de transporte de carga y transporte particular en el
-                    país, expertos en transporte de carga garantizando eficiencia,
-                    eficacia, seguridad y rentabilidad Empresa especializada en el
-                    transporte terrestre de carga.
-                  
-                </Col>
-             
-              </center>
-            </p>
-          </center>
-          <Row>
-            <Col>
-            <Card>
-               <center><img src={truck} alt="cargando..." className="imgIcons"/>
-              <h5 className="tema">Trasporte pesado</h5>
-
-              <p> se traen maquinaria para trabajo pesado como trailers, furgones y camiones</p>
-              </center> 
-              </Card>
-            </Col>
-            <Col>
-            <Card>
-               <center><img src={car} alt="cargando..." className="imgIcons"/>
-              <h5 className="tema">Trasporte liviano</h5>
-
-              <p> se traen maquinaria para trabajo pesado como trailers, furgones y camiones</p>
-              </center> 
-              </Card>
-            </Col>
-            <Col>
-                <Card>
-                  <center><img src={forklift} alt="cargando..." className="imgIcons"/>
-                  <h5 className="tema">Trasporte Industrial</h5>
-
-                  <p> se traen maquinaria para trabajo pesado como trailers, furgones y camiones</p>
-                  </center> 
-               </Card>
-            </Col>
-            <Col>
-            <Card>
-               <center><img src={towtruck} alt="cargando..." className="imgIcons"/>
-              <h5 className="tema">Servicio grua 24 Horas</h5>
-
-              <p> se traen maquinaria para trabajo pesado como trailers, furgones y camiones</p>
-              </center> 
-              </Card>
-            </Col>
-          </Row>
-        
+         <Row>
+          <center className="estandares">LOS MEJORES VEHICULOS.</center>
+         </Row>
         </Container>
       </div>
-      
+
       <Footer></Footer>
     </Layout>
-    
   );
 };
 
